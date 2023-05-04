bd6f93118e48a508acd296bcb5e3c26233d4bb83
sohelranagtc <sohelranagtc@gmail.com>
2023-05-04 21:50:34 +0600

    add active route

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,25 +1,27 @@
 import React, { useContext } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { FaUserCircle } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import "./Header.css"
 const Header = () => {
+    const location = useLocation();
 
     return (
         <div className='container mx-auto'>
             <Navbar className='rounded mt-2' bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand className='text-bold'> <img style={{width: '50px'}} src="../../public/logo.PNG" alt="" /> Chef <span className='text-warning'>Food</span> Hunter</Navbar.Brand>
+                    <Navbar.Brand className='text-bold'> <img style={{width: '50px'}} src="https://i.ibb.co/jL2SHH7/logo.png" alt="" /> Chef <span className='text-warning'>Food</span> Hunter</Navbar.Brand>
                     <div className='d-flex gap-5'>
                         <div className="d-flex gap-5 me-auto tex-center text-decoration-none">
-                            <Link className='text-decoration-none text-white' to="/">Home</Link>
-                            <Link className='text-decoration-none text-white' to="/blogs">Blogs</Link>
-                            <Link className='text-decoration-none text-white'>About</Link>
-                            <Link className='text-decoration-none text-white' to="/contact">Contact</Link>
+                            <Link className={`text-decoration-none text-warning ${location.pathname === '/' ? 'active' : ''}`} to="/">Home</Link>
+                            <Link className={`text-decoration-none text-warning ${location.pathname === '/blogs' ? 'active' : ''}`} to="/blogs">Blogs</Link>
+                            <Link className={`text-decoration-none text-warning ${location.pathname === '/about' ? 'active' : ''}`} to="/about">About</Link>
+                            <Link className={`text-decoration-none text-warning ${location.pathname === '/contact' ? 'active' : ''}`} to="/contact">Contact</Link>
                         </div>
                         <div>
                             <div className="me-auto text-decoration-none">
                                 <Link className='text-decoration-none text-white me-2'><FaUserCircle style={{ fontSize: '2rem' }}/></Link>
-                                <Link className='text-decoration-none text-white' to="/login">Login</Link>
+                                <Link className={`text-decoration-none text-white ${location.pathname === '/login' ? 'active' : ''}`} to="/login">Login</Link>
                             </div>
                         </div>
                     </div>
